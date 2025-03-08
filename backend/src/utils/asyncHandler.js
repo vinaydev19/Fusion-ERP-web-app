d9d520a99169a92420265005dea2ff80dd2c5bca
vinay1945 <vinaykumarporandla19@gmail.com>
2025-03-08 16:17:51 +0530

    asyncHandle code change

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,15 +1,18 @@
 import { ApiError } from "./ApiError.js";
 
-const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
-      if (!error.statusCode) {
-         error = new ApiError(500, "Internal Server Error");
-      }
+const asyncHandler = (fn) => async (...args) => {
+  try {
+    await fn(...args);
+  } catch (error) {
+    console.log(error);
+    if (!error.statusCode) {
+      error = new ApiError(500, "Internal Server Error");
+    }
+    const next = args[2]; // Extract next from args
+    if (typeof next === "function") {
       next(error);
-    });
-  };
+    }
+  }
 };
 
 export { asyncHandler };
-
